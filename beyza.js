dec9f1d459c488a417a8786434267ab0e7cb99d1
Beyza Hız <beyzahiz.03@gmail.com>
2025-05-17 00:08:14 +0300

    liste

--- beyza.js
+++ beyza.js
@@ -1,5 +1,6 @@
 const addForm=document.querySelector('.add');
 const list=document.querySelector('.todos');
+const search=document.querySelector('.search input');
 
 const generateTemplate = (todo) =>{
     const html=`
@@ -31,3 +32,22 @@ list.addEventListener('click', e=>{
         e.target.parentElement.remove();
     }
 })
+
+const filterTodos = (term) =>{
+   /* console.log(term);
+    console.log(Array.from(list.children)); */
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+}
+
+search.addEventListener('keyup',()=>{
+    const term=search.value.trim().toLowerCase();
+   // console.log(term);
+    filterTodos(term);
+})
+
